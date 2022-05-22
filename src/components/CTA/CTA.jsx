5111f29cf80e869eb48f0580a4dd73bb98f5b769
Franklin Wagbara <wagbarafranklin@yahoo.com>
2022-05-22 10:12:42 +0100

    modified portfolio component

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,14 +1,31 @@
 import "./CTA.scss";
+import { PropTypes } from "prop-types";
 
-const CTA = (props) => {
+const CTA = ({
+  download = false,
+  label1 = "Download CV",
+  label2 = "Let's Talk",
+  action1 = "./CTA.scss",
+  action2,
+}) => {
   return (
     <div className="cta">
-      <a href="./CTA.scss" className="btn" download>
-        Download CV
+      <a href={action1} className="btn" download={download}>
+        {label1}
       </a>
-      <button className="btn btn-primary">Let's Talk</button>
+      <a href={action2} className="btn btn-primary">
+        {label2}
+      </a>
     </div>
   );
+};
+
+CTA.propTypes = {
+  download: PropTypes.bool.isRequired,
+  action1: PropTypes.string.isRequired,
+  action2: PropTypes.string.isRequired,
+  label1: PropTypes.string.isRequired,
+  label2: PropTypes.string.isRequired,
 };
 
 export default CTA;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,87 @@
 import "./Portfolio.scss";
+import Project1 from "../../assets/images/projects/project1.jpg";
+import CTA from "../CTA/CTA";
+
+const projects = [
+  {
+    id: 1,
+    image: Project1,
+    heading: "Real Estate website for a reputable real estate company",
+    gitHub: "https://github.com/franklinwagbara/Portfolio",
+    projectURL: "https://github.com/franklinwagbara/Portfolio",
+  },
+  {
+    id: 2,
+    image: Project1,
+    heading: "Real Estate website for a reputable real estate company",
+    gitHub: "https://github.com/franklinwagbara/Portfolio",
+    projectURL: "https://github.com/franklinwagbara/Portfolio",
+  },
+  {
+    id: 3,
+    image: Project1,
+    heading: "Real Estate website for a reputable real estate company",
+    gitHub: "https://github.com/franklinwagbara/Portfolio",
+    projectURL: "https://github.com/franklinwagbara/Portfolio",
+  },
+  {
+    id: 4,
+    image: Project1,
+    heading: "Real Estate website for a reputable real estate company",
+    gitHub: "https://github.com/franklinwagbara/Portfolio",
+    projectURL: "https://github.com/franklinwagbara/Portfolio",
+  },
+  {
+    id: 5,
+    image: Project1,
+    heading: "Real Estate website for a reputable real estate company",
+    gitHub: "https://github.com/franklinwagbara/Portfolio",
+    projectURL: "https://github.com/franklinwagbara/Portfolio",
+  },
+  {
+    id: 6,
+    image: Project1,
+    heading: "Real Estate website for a reputable real estate company",
+    gitHub: "https://github.com/franklinwagbara/Portfolio",
+    projectURL: "https://github.com/franklinwagbara/Portfolio",
+  },
+];
 
 const Portfolio = (props) => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <Projects projects={projects} />
+    </section>
+  );
 };
 export default Portfolio;
+
+const Projects = ({ projects }) => {
+  return (
+    <div className="projects__container">
+      {projects.map((project) => (
+        <Project key={project.id} project={project} />
+      ))}
+    </div>
+  );
+};
+
+const Project = ({ project }) => {
+  return (
+    <article className="project__container">
+      <div className="project__image">
+        <img src={project.image} alt="Project" />
+      </div>
+      <div className="info__wrapper">
+        <h3>{project.heading}</h3>
+        <CTA
+          download={false}
+          label1="GitHub"
+          label2="Live Demo"
+          action1={project.gitHub}
+          action2={project.projectURL}
+        />
+      </div>
+    </article>
+  );
+};
